ee80cdc65d39719a6aad6653df55be7195af9f9e
Ling <asklingtoby@gmail.com>
2016-06-13 17:04:12 -0400

    set up storyboard

---
+++ TheGreatHunt/welcomeScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  welcomeScreenViewController.h
+//  TheGreatHunt
+//
+//  Created by ling toby on 6/13/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface welcomeScreenViewController : UIViewController
+
+@end
